214056f0b130be732dd3988420c03d1995791714
Ryougi0 <ronitodubon@gmail.com>
2025-02-21 08:05:51 -0500

    Codeathon.js

--- Codeathon.js
+++ Codeathon.js
@@ -1,30 +1,28 @@
 function enterLibrary() {
-    // Play click sound
-    document.getElementById('click-sound').play();
- 
- 
-    // Hide popup
-    document.getElementById('popup').style.display = 'none';
- 
- 
-    // Show journal and play background music
-    const journal = document.getElementById('journal');
-    journal.style.display = 'block';
-    document.getElementById('background-music').play();
- }
- 
- 
- function flipCard(card) {
-    card.classList.toggle('flipped');
- }
- 
- 
- function setVolume(value) {
-    document.getElementById('background-music').volume = value;
- }
- 
- 
- function toggleMute() {
-    const backgroundMusic = document.getElementById('background-music');
-    backgroundMusic.muted = !backgroundMusic.muted;
- }
+   // Plays click sound
+   document.getElementById('click-sound').play();
+
+
+   document.getElementById('popup').style.display = 'none';
+
+
+   const journal = document.getElementById('journal');
+   journal.style.display = 'block';
+   document.getElementById('background-music').play();
+}
+
+
+function flipCard(card) {
+   card.classList.toggle('flipped');
+}
+
+
+function setVolume(value) {
+   document.getElementById('background-music').volume = value;
+}
+
+
+function toggleMute() {
+   const backgroundMusic = document.getElementById('background-music');
+   backgroundMusic.muted = !backgroundMusic.muted;
+}
